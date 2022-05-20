eeaf013cafde226f46edf03f7f97e947f9628026
luisgs7 <luisgsilva270@gmail.com>
2022-05-20 16:13:43 -0300

    remove SECRET_KEY

--- app/app/settings.py
+++ app/app/settings.py
@@ -10,7 +10,14 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
+import dotenv
 from pathlib import Path
+
+dotenv.load_dotenv(dotenv.find_dotenv())
+
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +27,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-mha(@+-_(ye4^@w4@m3(&uw%-+sj0mvpm3=-q!@b2a%^^c$km*'
-
+SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
