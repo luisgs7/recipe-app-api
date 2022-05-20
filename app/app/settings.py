b053ef7f41980678fb7707038421d3b54197ae1b
Luís Gustavo <luisgsilva270@gmail.com>
2022-05-20 20:39:04 -0300

    Update settings.py

--- app/app/settings.py
+++ app/app/settings.py
@@ -16,23 +16,19 @@ from pathlib import Path
 
 dotenv.load_dotenv(dotenv.find_dotenv())
 
-
-
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("SECRET_KEY")
+
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = True if os.environ.get('DEBUG') == '1' else False
 
-ALLOWED_HOSTS = ['*']
-
+ALLOWED_HOSTS: list[str] = [os.environ.get('ALLOWED_HOSTS')]
 
 # Application definition
 
