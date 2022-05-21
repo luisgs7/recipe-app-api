c0271a0f125e13a14fe0af7e41220dea89a25895
luisgs7 <luisgsilva270@gmail.com>
2022-05-21 09:37:37 -0300

    remove secret_key

--- app/app/settings.py
+++ app/app/settings.py
@@ -23,7 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-mha(@+-_(ye4^@w4@m3(&uw%-+sj0mvpm3=-q!@b2a%^^c$km*' #os.getenv("SECRET_KEY")
+#SECRET_KEY = 'django-insecure-mha(@+-_(ye4^@w4@m3(&uw%-+sj0mvpm3=-q!@b2a%^^c$km*' #os.getenv("SECRET_KEY")
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True if os.environ.get('DEBUG') == '1' else False
