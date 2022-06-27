fed70b2cf6c128b600959ae545e2987d2851b615
luisgs7 <luisgsilva270@gmail.com>
2022-06-27 15:20:06 -0300

    flake8 - ok

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -15,7 +15,7 @@ class ModelTests(TestCase):
         user = get_user_model().objects.create_user(
             email=email,
             password=password,
-        )        
+        )
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
